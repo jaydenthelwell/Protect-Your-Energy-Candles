2e7b49d6821199ebd745696ce9fe8785aae73721
jaydenthelwell <jaydenthelwell02@gmail.com>
2023-11-15 18:06:18 +0000

    Revert "Sign Up"
    This reverts commit 9b55fa69ff41c2b044551bdd95e8326a6a20999a.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :users
-
   root to: 'home#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :candles, only: [:index, :show]
@@ -8,7 +6,9 @@ Rails.application.routes.draw do
   resources :editions do
     get 'light', on: :collection
     get 'dark', on: :collection
+
   end
+
   # Defines the root path route ("/")
   # root "articles#index"
 end
