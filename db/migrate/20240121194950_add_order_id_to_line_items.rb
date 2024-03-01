49c24f8c3ce5e81613edc19dfecccb93272966d9
jaydenthelwell <jaydenthelwell02@gmail.com>
2024-03-01 11:51:55 +0000

    Remopving duplicate order_id column

--- db/migrate/20240121194950_add_order_id_to_line_items.rb
+++
@@ -1,5 +0,0 @@
-class AddOrderIdToLineItems < ActiveRecord::Migration[7.0]
-  def change
-    add_column :line_items, :order_id, :integer
-  end
-end
--- db/migrate/20240301114004_remove_duplicate_order_id_from_line_items.rb
+++
@@ -1,5 +0,0 @@
-class RemoveDuplicateOrderIdFromLineItems < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :line_items, :order_id
-  end
-end
