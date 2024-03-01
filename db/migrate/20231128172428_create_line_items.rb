cc69781e1c3de981c4410bc9440deccb19ab3535
jaydenthelwell <jaydenthelwell02@gmail.com>
2024-03-01 11:33:31 +0000

    Remove line-items from cartstable delted

--- db/migrate/20231128172428_create_line_items.rb
+++ db/migrate/20231128172428_create_line_items.rb
@@ -3,12 +3,7 @@ class CreateLineItems < ActiveRecord::Migration[7.0]
     create_table :line_items do |t|
       t.references :candle, null: false, foreign_key: true
       t.references :cart, null: false, foreign_key: true
-# In your migration file
-remove_foreign_key :line_items, :carts
-
       t.timestamps
     end
   end
 end
-# In your migration file
-remove_foreign_key :line_items, :carts
--- db/migrate/20240122114901_remove_foreign_key_from_line_items.rb
+++
@@ -1,5 +0,0 @@
-class RemoveForeignKeyFromLineItems < ActiveRecord::Migration[7.0]
-  def change
-    remove_foreign_key :line_items, :carts
-  end
-end
