d7d9893fef7152c1d6f19d7e68a17c8c3d5482d5
jaydenthelwell <jaydenthelwell02@gmail.com>
2024-03-07 21:11:03 +0000

    User ediyt page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,23 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!, only: [:show]
+  before_action :authenticate_user!, only: [:show, :update]
 
   def show
     @user = current_user
-    # @address = @user.addresses.first
+    @address = @user.addresses.first
+  end
+
+  def update
+    @user = current_user
+    if @user.update(user_params)
+      redirect_to @user, notice: 'Profile was successfully updated.'
+    else
+      render :show
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :second_name, :email, :password, :password_confirmation, :d_o_b)
   end
 end
