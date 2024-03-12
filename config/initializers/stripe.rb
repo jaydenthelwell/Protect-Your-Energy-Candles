979a60ed76d93ad359a6892e1cb009a1075991b9
jaydenthelwell <jaydenthelwell02@gmail.com>
2024-03-12 10:21:21 +0000

    changed the keys

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -2,8 +2,8 @@
 #   :publishable_key => ENV['STRIPE_PUBLISHABLE_KEY'],
 #   :secret_key => ENV['STRIPE_SECRET_KEY']
 # }
-# Stripe.api_key = Rails.application.credentials.dig(:stripe, :secret_key)
-Stripe.api_version = "2023-10-16"
+# # Stripe.api_key = Rails.application.credentials.dig(:stripe, :secret_key)
+# Stripe.api_version = "2023-10-16"
 # Rails.configuration.stripe = {
 #   publishable_key: ENV['STRIPE_PUBLISHABLE_KEY'],
 #   secret_key: ENV['STRIPE_SECRET_KEY']
