cae4ee2602463736b28c13f662595f617101a6d5
jaydenthelwell <jaydenthelwell02@gmail.com>
2024-03-24 19:22:34 +0000

    Console logged

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,6 +1,6 @@
 import { Application } from "@hotwired/stimulus";
 import { StripeController } from "./stripe_controller.js";
-
+console.log("This is application.js")
 const application = Application.start();
 application.register("stripe", StripeController);
 application.debug = false;
