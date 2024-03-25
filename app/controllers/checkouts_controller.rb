4e6040b7a9427e43493a8eb91d5261631b0dfacb
jaydenthelwell <jaydenthelwell02@gmail.com>
2024-03-25 10:38:40 +0000

    production assets precompile

--- app/controllers/checkouts_controller.rb
+++ app/controllers/checkouts_controller.rb
@@ -1,7 +1,5 @@
 class CheckoutsController < ApplicationController
   def show
-    Stripe.api_key = Rails.application.credentials.dig(:stripe, :secret_key)
-    Stripe.api_version = "2023-10-16"
     @session = Stripe::Checkout::Session.create(
       line_items: [{
         price_data: {
