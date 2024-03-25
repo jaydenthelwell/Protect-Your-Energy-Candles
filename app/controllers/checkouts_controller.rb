abdbf74f3e00a116bd42d0dedbe66716a2a692ec
jaydenthelwell <jaydenthelwell02@gmail.com>
2024-03-25 08:53:02 +0000

    Removed typo

--- app/controllers/checkouts_controller.rb
+++ app/controllers/checkouts_controller.rb
@@ -1,5 +1,7 @@
 class CheckoutsController < ApplicationController
   def show
+    Stripe.api_key = Rails.application.credentials.dig(:stripe, :secret_key)
+    Stripe.api_version = "2023-10-16"
     @session = Stripe::Checkout::Session.create(
       line_items: [{
         price_data: {
